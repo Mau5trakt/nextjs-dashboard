0ed360e32158781e842fa5de59b93811b8b28c03
Enrique Mauricio Alemany Torres <80871251+Mau5trakt@users.noreply.github.com>
2026-01-25 05:17:21 +0000

    Chapter 14:  add login and logout functionalities

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        'humble-space-dollop-7x4v6x6rg6x2p67g-3000.app.github.dev', 
+        'localhost:3000'
+      ],
+    },
+  },
   /* config options here */
 };
 
